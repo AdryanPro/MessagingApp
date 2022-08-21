cf28caee57dc1ab26882a32c84e3ad734460ddc1
Adryan Cadet <adryan-pro@hotmail.com>
2022-08-22 00:49:11 +0200

    Auth done

--- firebase.js
+++ firebase.js
@@ -1,3 +1,8 @@
+import * as firebase from "firebase";
+import  "firebase/auth";
+import  "firebase/firestore";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCHfcBjcOTFJbzjNP7UaGtAAYb2lMOm2YA",
     authDomain: "mymessaginapp-73979.firebaseapp.com",
@@ -6,3 +11,16 @@ const firebaseConfig = {
     messagingSenderId: "887843271775",
     appId: "1:887843271775:web:aa945f7d2fc57ced2dd7ea"
 };
+
+let app;
+
+if(firebase.apps.lenght === 0){
+    app = firebase.initializeApp(firebaseConfig);
+} else {
+    app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth};
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, KeyboardAvoidingView } from 'react-native'
 import { Button, Input, Image } from '@rneui/base'
 import { StatusBar } from 'expo-status-bar'
+import { auth } from '../firebase'
 
 const LoginScreen = ({ navigation }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     
+    useEffect(() =>{
+        const unsubscribe = auth.onAuthStateChanged((authUser) =>{
+            if(authUser){
+                navigation.replace('Home');
+            }
+        });
+
+        return unsubscribe;
+    }, []);
+
     const signIn = () =>{}
   return (
     <KeyboardAvoidingView behavior='padding'  style={styles.container}>
